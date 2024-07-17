679f087206a17a6e8bd2af58220b016cfd9a461d
olafenko <olopikolo666@gmail.com>
2024-07-17 17:36:43 +0200

    simple requests authorization to allow only admin to use individual methods

--- src/main/java/org/backend/gamelibwebapp/repositories/AppUserRepository.java
+++ src/main/java/org/backend/gamelibwebapp/repositories/AppUserRepository.java
@@ -4,8 +4,6 @@ import org.backend.gamelibwebapp.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser,Long> {
 
