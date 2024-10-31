b43d51f11951346af82e45d6d7e02243bf8d9e6c
olafenko <olopikolo666@gmail.com>
2024-10-31 13:42:47 +0100

    wrote test for add game but need to fix conversion error for enum to H2 db

--- src/test/java/org/backend/gamelibwebapp/controller/GameControllerTest.java
+++ src/test/java/org/backend/gamelibwebapp/controller/GameControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -27,6 +28,22 @@ class GameControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private GameRepository gameRepository;
+
+    @Test
+    void should_add_new_game() throws Exception {
+
+        //given
+
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/games/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\":\"Test game\",\"producer\":\"Test producer\",\"gameCategories\":[\"RPG\"],\"imageUrl\":\"test\"}"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Test game"));
+        //then
+
+    }
 
 
     @Test
